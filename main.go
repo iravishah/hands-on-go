fadb58fc358dfb64cd021adeb5de7dd98d68ae76
RAVS <ravs@eur.ad.sag@SAG-D99KMQ2-U.EUR.AD.SAG>
2020-04-22 19:57:12 +0530

    Worker on create, get single and delete routes

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -10,9 +11,10 @@ import (
 )
 
 type Article struct {
-	Title   string `json:"Title"`
-	Desc    string `json:"desc"`
-	Content string `json:"content"`
+	Id      string
+	Title   string
+	Desc    string
+	Content string
 }
 
 var Articles []Article
@@ -25,8 +27,8 @@ func main() {
 
 func loadArticals() {
 	Articles = []Article{
-		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-		Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+		Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
 	}
 }
 
@@ -40,9 +42,43 @@ func getAllArticles(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(Articles)
 }
 
+func getArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	for _, article := range Articles {
+		if article.Id == key {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
+}
+
+func createArticle(w http.ResponseWriter, r *http.Request) {
+	body, _ := ioutil.ReadAll(r.Body)
+	var article Article
+	json.Unmarshal(body, &article)
+	Articles = append(Articles, article)
+	json.NewEncoder(w).Encode(article)
+}
+
+func removeArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	for index, article := range Articles {
+		if article.Id == key {
+			Articles = append(Articles[:index], Articles[index+1:]...)
+		}
+	}
+}
+
 func handlerRequests() {
 	apiRouter := mux.NewRouter().StrictSlash(true)
 	apiRouter.HandleFunc("/", homepage)
-	apiRouter.HandleFunc("/articles", getAllArticles)
+	apiRouter.HandleFunc("/articles", getAllArticles).Methods("GET")
+	apiRouter.HandleFunc("/articles/{id}", getArticle).Methods("GET")
+	apiRouter.HandleFunc("/article", createArticle).Methods("POST")
+	apiRouter.HandleFunc("/article/{id}", removeArticle).Methods("DELETE")
+
 	log.Fatal(http.ListenAndServe(":9999", apiRouter))
 }
